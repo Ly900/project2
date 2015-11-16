ddafa6a83504667480b92bcbd0c2323555fac86c
Ly Nguyen <Ly900@aol.com>
2015-11-16 11:41:21 -0500

    add edit and delete buttons for student

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,21 +5,37 @@ class StudentsController < ApplicationController
   end
 
   def show
-
+    @student = Student.find(params[:id])
   end
 
   def new
     @student = Student.new
   end
 
-  # def create
-  #   @student = Student.create!(student_params)
-  #   redirect_to students_index_path
-  # end
-  #
-  # private
-  #   def student_params
-  #     params.require(:student).permit(:name, :date_of_birth, :address, :phone_number, :email_address)
-  #   end
+  def create
+    @student = Student.create!(student_params)
+    redirect_to students_path
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    @student.update!(student_params)
+    redirect_to students_path
+  end
+
+  def destroy
+    @student = Student.find(params[:id])
+    @student.destroy
+    redirect_to students_path
+  end
+
+  private
+    def student_params
+      params.require(:student).permit(:name, :date_of_birth, :address, :phone_number, :email_address)
+    end
 
 end
