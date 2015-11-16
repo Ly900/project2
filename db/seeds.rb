5301ec5095c6c197bd9f78d062739c880d38aa15
Ly Nguyen <Ly900@aol.com>
2015-11-16 13:07:34 -0500

    used date_field for date of birth form field to get correct date format

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,11 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 Student.destroy_all
 
-Student.create(name: "Andy Anderson", date_of_birth: "1/1/2005", address: "123 Bird Lane, Washington DC, 20010", phone_number: "202-123-4567", email_address: "andy.anderson@yahoo.com")
+Student.create(name: "Andy Anderson", date_of_birth: "1/2/2005", address: "123 Bird Lane, Washington DC, 20010", phone_number: "202-123-4567", email_address: "andy.anderson@yahoo.com")
 
-Student.create(name: "Beatrice Bunion", date_of_birth: "2/2/2005", address: "123 Cat Street, Washington, DC 20011", phone_number: "202-234-5678", email_address: "b.bunion@gmail.com")
+Student.create(name: "Beatrice Bunion", date_of_birth: "2/3/2005", address: "123 Cat Street, Washington, DC 20011", phone_number: "202-234-5678", email_address: "b.bunion@gmail.com")
 
-Student.create(name: "Carol Cluster", date_of_birth: "3/3/2005", address: "123 Dog Circle, Washington, DC 200012", phone_number: "202-345-6789", email_address: "carol.cluster@aol.com")
+Student.create(name: "Carol Cluster", date_of_birth: "3/4/2005", address: "123 Dog Circle, Washington, DC 200012", phone_number: "202-345-6789", email_address: "carol.cluster@aol.com")
 
 Student.create(name: "Dunkin Donuts", date_of_birth: "4/4/2005", address: "123 Rabbit Boulevard, Washington, DC 20013", phone_number: "202-456-7890", email_address: "dunkin.d@aol.com")
 
