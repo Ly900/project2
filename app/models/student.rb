5526cc0b9b214f9dd69191589380d5e6ee7c5b00
Ly Nguyen <Ly900@aol.com>
2015-11-17 20:09:32 -0500

    make existence of students dependent on existence of user in model and add seed data for users and their students

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,4 @@
 class Student < ActiveRecord::Base
-  belongs_to :user
+  belongs_to :user, dependent: :destroy
   has_many :logs, dependent: :destroy
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,24 +8,28 @@
 # User.destroy_all
 User.destroy_all
 Student.destroy_all
-Log.destroy_all
+Log.destroy_all 
 
-users = User.create!([
-  {email: "Teacher1@email.com", password: "password", password_confirmation: "password"},
-  {email: "Teacher2@email.com", password: "password", password_confirmation: "password"},
-  {email: "Teacher3@email.com", password: "password", password_confirmation: "password"},
+User.create!([
+  {email: "teacher1@email.com", password: "password", password_confirmation: "password"},
+  {email: "teacher2@email.com", password: "password", password_confirmation: "password"},
+  {email: "teacher3@email.com", password: "password", password_confirmation: "password"},
 ])
 
-andy = Student.create(first_name: "Andy", last_name: "Anderson", date_of_birth: "1/2/2005", address: "123 Bird Lane, Washington DC, 20010", phone_number: "202-123-4567", email_address: "andy.anderson@yahoo.com")
+teacher1 = User.find_by(email:"teacher1@email.com")
+teacher1.students.create([
+  {first_name: "Andy", last_name: "Anderson", date_of_birth: "1/2/2005", address: "123 Bird Lane, Washington DC, 20010", phone_number: "202-123-4567", email_address: "andy.anderson@yahoo.com"},
+  {first_name: "Beatrice", last_name: "Bunion", date_of_birth: "2/3/2005", address: "123 Cat Street, Washington, DC 20011", phone_number: "202-234-5678", email_address: "b.bunion@gmail.com"},
+  {first_name: "Carol", last_name: "Cluster", date_of_birth: "3/4/2005", address: "123 Dog Circle, Washington, DC 200012", phone_number: "202-345-6789", email_address: "carol.cluster@aol.com"}
+])
 
-andy.logs.create(date_of_contact: "5/3/2015", mode_of_contact: "Phonecall", type_of_contact: "Positive", contact_details: "Called to tell Andy's mom that he has been behaving very well this week and turned in all his homework.")
+teacher2 = User.find_by(email:"teacher2@email.com")
+teacher2.students.create([
+  {first_name: "Dunkin", last_name: "Donuts", date_of_birth: "4/4/2005", address: "123 Rabbit Boulevard, Washington, DC 20013", phone_number: "202-456-7890", email_address: "dunkin.d@aol.com"},
+  {first_name: "Eric", last_name: "Evil", date_of_birth: "5/5/2005", address: "123 Pidegon Road, Washington, DC 20014", phone_number: "202-567-8901", email_address: "eric.evil@hotmail.com"}
+])
 
-andy.logs.create(date_of_contact: "5/4/2015", mode_of_contact: "Email", type_of_contact: "Negative", contact_details: "Lying in class.")
-
-Student.create(first_name: "Beatrice", last_name: "Bunion", date_of_birth: "2/3/2005", address: "123 Cat Street, Washington, DC 20011", phone_number: "202-234-5678", email_address: "b.bunion@gmail.com")
-
-Student.create(first_name: "Carol", last_name: "Cluster", date_of_birth: "3/4/2005", address: "123 Dog Circle, Washington, DC 200012", phone_number: "202-345-6789", email_address: "carol.cluster@aol.com")
-
-Student.create(first_name: "Dunkin", last_name: "Donuts", date_of_birth: "4/4/2005", address: "123 Rabbit Boulevard, Washington, DC 20013", phone_number: "202-456-7890", email_address: "dunkin.d@aol.com")
-
-Student.create(first_name: "Eric", last_name: "Evil", date_of_birth: "5/5/2005", address: "123 Pidegon Road, Washington, DC 20014", phone_number: "202-567-8901", email_address: "eric.evil@hotmail.com")
+#
+# andy.logs.create(date_of_contact: "5/3/2015", mode_of_contact: "Phonecall", type_of_contact: "Positive", contact_details: "Called to tell Andy's mom that he has been behaving very well this week and turned in all his homework.")
+#
+# andy.logs.create(date_of_contact: "5/4/2015", mode_of_contact: "Email", type_of_contact: "Negative", contact_details: "Lying in class.")
